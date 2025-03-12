b3c0fbe3e576dcec70a319f8794465c9789380e3
Nikolay16349 <kolyazverev99@mail.ru>
2025-03-12 14:51:25 +0800

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 300;
 
+// Персонаж
 let player = { 
     x: 50, 
     y: 200, 
@@ -13,12 +14,14 @@ let player = {
     jumping: false 
 };
 
+// Настройки
 let obstacles = [];
 let gameSpeed = 3;
 let gravity = 0.5;
 let jumpPower = -10;
 let groundLevel = canvas.height - 50;
-let obstacleSpawnCooldown = 100; 
+let obstacleTimer = 0;
+let obstacleInterval = 120; // Чем больше значение, тем реже препятствия
 
 document.addEventListener("keydown", (e) => {
     if (e.code === "Space" && !player.jumping) {
@@ -28,23 +31,28 @@ document.addEventListener("keydown", (e) => {
 });
 
 function update() {
+    // Применяем гравитацию
     player.y += player.dy;
     player.dy += gravity;
 
+    // Ограничение, чтобы не проваливался под землю
     if (player.y + player.height >= groundLevel) {
         player.y = groundLevel - player.height;
         player.dy = 0;
         player.jumping = false;
     }
 
+    // Двигаем препятствия
     for (let i = 0; i < obstacles.length; i++) {
         obstacles[i].x -= gameSpeed;
 
+        // Удаляем, если препятствие вышло за экран
         if (obstacles[i].x + obstacles[i].width < 0) {
             obstacles.splice(i, 1);
             i--;
         }
 
+        // Проверяем столкновение
         if (
             player.x < obstacles[i].x + obstacles[i].width &&
             player.x + player.width > obstacles[i].x &&
@@ -56,23 +64,27 @@ function update() {
         }
     }
 
-    if (obstacleSpawnCooldown <= 0) {
+    // Создаём препятствия с нормальным интервалом
+    if (obstacleTimer <= 0) {
         obstacles.push({ x: canvas.width, y: groundLevel - 30, width: 20, height: 30 });
-        obstacleSpawnCooldown = 150;
+        obstacleTimer = obstacleInterval; // Сброс таймера
     } else {
-        obstacleSpawnCooldown--;
+        obstacleTimer--; // Уменьшаем таймер
     }
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Рисуем землю
     ctx.fillStyle = "green";
     ctx.fillRect(0, groundLevel, canvas.width, 50);
 
+    // Рисуем персонажа
     ctx.fillStyle = "red";
     ctx.fillRect(player.x, player.y, player.width, player.height);
 
+    // Рисуем препятствия
     ctx.fillStyle = "black";
     for (let obstacle of obstacles) {
         ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
